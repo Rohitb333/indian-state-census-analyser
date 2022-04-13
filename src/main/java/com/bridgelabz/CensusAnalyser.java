70cddcc81bf74d68f259c57c65c6a8936d366c61
Rohit <rohitb333@gmail.com>
2022-04-13 17:43:43 +0530

    Added file

---
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Purpose : To implement Indian State Census Analyser
+ *
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-09-29
+ */
+public class CensusAnalyser {
+    public static void main(String[] args) throws IOException, StateSensorAnalyserException {
+        System.out.println("Welcome To The Program");
+        String filePathRead = "C:\\Users\\Rohit\\IdeaProjects\\indian-state-census-analyser\\src\\main\\resources\\statewise-census-data-in-india-1901-2011";
+        String fileName = "statewise-census-data-in-india-1901-2011";
+        String delimiter =",";
+        List<String> stringName = new ArrayList<>();
+        stringName.add("State");
+        stringName.add("Population");
+        stringName.add("AreaInSqKm");
+        stringName.add("DensityPerSqKm");
+
+        ReadOperations readObj = new ReadOperations();
+        int count = readObj.readDataCount(filePathRead, fileName);
+        System.out.println(count);
+
+        readObj.readDelimiter(filePathRead, delimiter);
+        readObj.readHeader(filePathRead, stringName);
+    }
+}
